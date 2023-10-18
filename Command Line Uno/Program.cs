e9817a78de485575661290bfa768b904af8ff658
triscrpt <xoeaxx@gmail.com>
2023-10-18 22:07:55 +0100

    Add project files.

---
+++ Command Line Uno/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Line_Uno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
